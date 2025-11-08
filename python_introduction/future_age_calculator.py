2c01178505920e8374fb70299e8392b84a3fc634
Opwondo <opwondow@gmail.com>
2025-11-08 19:51:51 +0300

    Added a file future_age_calculator.py that calculates the future age of a person

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+current_age = int(input("How old are you? "))
+future_year = int(input("Enter a future year: "))
+current_year = 2023
+years_difference = future_year - current_year
+future_age = current_age + years_difference
+print(f"In {future_year}, you will be {future_age} years old.")
