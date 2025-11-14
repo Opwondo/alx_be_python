e3da2f9d811b9c587490e1debdf81db7e56f2e80
Opwondo <opwondow@gmail.com>
2025-11-15 01:16:40 +0300

    Added a file match_case_calculator.py that performs Addition, subtraction, Multiplication and Division

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Choose the operation (+, -, *, /):").strip().lower()
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result is {result}.")
+        else:
+            print("Cannot divide by zero.")    
+    case _:
+        print("Invalid operation selected. Please choose add, subtract, multiply, or divide.") 
+        
