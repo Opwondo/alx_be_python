3bbad6efddc015523fd9fb37f0423a6715157c99
Opwondo <opwondow@gmail.com>
2025-11-15 21:35:11 +0300

    Added a file multiplication_table.py that creates a multiplication table when the user enters a certain number

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,15 @@
+"""
 current_age = int(input("How old are you? "))
 future_age = current_age + 27
 print(f"In 2050, you will be ", future_age, " years old.")
+"""
+rows = 5
+
+i = 0
+while i <= rows:
+    j = 0
+    while j <= i:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1
