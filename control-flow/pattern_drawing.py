49a1727142a878abe14e63a1c69b061bc37495b9
Opwondo <opwondow@gmail.com>
2025-11-15 22:18:57 +0300

    Added a file pattern_drawing.py that draws a pattern based on the number input from the user

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+pattern_size = input("Enter the size of the pattern: ")
+while not pattern_size.isdigit() or int(pattern_size) <= 0:
+    pattern_size = input("Please enter a valid positive integer for the size of the pattern: ")
+pattern_size = int(pattern_size)
+for i in range(pattern_size):
+    for j in range(pattern_size):
+        print("*", end=" ")
+    print()
+    
